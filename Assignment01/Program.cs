48f5f33655fd3e87e77bef75b1f914b671ee7277
moohammedddd <midorafat115566@gmail.com>
2025-02-23 01:21:57 +0200

    Add project files.

---
+++ Assignment01/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using LINQ;
+using System.Net.Http.Headers;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using static LINQ.ListGenerator;
+
+namespace Assignment01
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}

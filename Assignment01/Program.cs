4fd3adeb977affe5c86d784f992c39300ee2f74f
moohammedddd <midorafat115566@gmail.com>
2025-02-23 01:58:26 +0200

    Transformation Operators Q6

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -104,19 +104,27 @@ namespace Assignment01
             #endregion
 
             #region Q5
-            int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
-            int[] numbersB = { 1, 3, 5, 7, 8 };
+            //int[] numbersA = { 0, 2, 4, 5, 6, 8, 9 };
+            //int[] numbersB = { 1, 3, 5, 7, 8 };
 
-            var pairs = from a in numbersA
-                        from b in numbersB
-                        where a < b
-                        select (a, b);
+            //var pairs = from a in numbersA
+            //            from b in numbersB
+            //            where a < b
+            //            select (a, b);
 
-            Console.WriteLine("Pairs where a < b:");
-            foreach (var pair in pairs)
-            {
-                Console.WriteLine($"{pair.a} is less than {pair.b}");
-            }
+            //Console.WriteLine("Pairs where a < b:");
+            //foreach (var pair in pairs)
+            //{
+            //    Console.WriteLine($"{pair.a} is less than {pair.b}");
+            //}
+            #endregion
+
+            #region Q6
+            Console.WriteLine("select Orders ");
+            var result = from a in CustomerList
+                         from o in a.Orders
+                         where o.Total < 500.00m
+                         select o;
             #endregion
         }
     }
